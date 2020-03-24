41a362ddffa895e2e45b0c691980a8b7bf7f1089
Kaze <renyuan7777@gmail.com>
2020-03-24 10:04:49 -0700

    Functional at least

---
+++ app/src/main/java/com/example/gitdrugged/NotifReceiver.java
@@ -0,0 +1,17 @@
+package com.example.gitdrugged;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class NotifReceiver extends BroadcastReceiver {
+    public NotifReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Intent intent1 = new Intent(context, NotifIntentService.class);
+        context.startService(intent1);
+    }
+}
