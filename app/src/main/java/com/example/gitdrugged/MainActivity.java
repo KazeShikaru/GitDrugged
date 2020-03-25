037b4697f9190c61622d479bea187c561edf09a8
Kaze <renyuan7777@gmail.com>
2020-03-24 20:02:45 -0700

    No idea what I changed

--- app/src/main/java/com/example/gitdrugged/MainActivity.java
+++ app/src/main/java/com/example/gitdrugged/MainActivity.java
@@ -1,32 +1,55 @@
 package com.example.gitdrugged;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import android.app.AlarmManager;
+import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
+import static com.example.gitdrugged.App.CHANNEL_1_ID;
+import static com.example.gitdrugged.App.CHANNEL_2_ID;
 public class MainActivity extends AppCompatActivity {
-    public static final int REQUESTCODE = 1;
+    private NotificationManagerCompat notificationManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        notificationManager = NotificationManagerCompat.from(this);
+
+
     }
 
-    public void setAlarm(View view){
-        Intent notifyIntent = new Intent(this,NotifReceiver.class);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast
-                (view.getContext(), REQUESTCODE, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        AlarmManager alarmManager = (AlarmManager) view.getContext().getSystemService(Context.ALARM_SERVICE);
-        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,  System.currentTimeMillis(),
-                1000 * 60 * 60 * 24, pendingIntent);
+    public void sendOnChannel1(View v) {
+        String title = "Git Drugged reminder";
+        String message = "Let's do drugs now!";
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_1_ID)
+                .setSmallIcon(R.drawable.gitdruggedlogo)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+
+        notificationManager.notify(1, notification);
     }
 
+
+    public String getData(){
+
+        return "drug1,d,14,drug2,w,2";
+
+    }
 
 
 }
