fc44c38142d844e4cfe3ebf24e9750fbbb4a5a9b
Kaze <renyuan7777@gmail.com>
2020-03-25 21:13:25 -0700

    default list added

--- app/src/main/java/com/example/gitdrugged/ReminderReturn.java
+++ app/src/main/java/com/example/gitdrugged/ReminderReturn.java
@@ -18,6 +18,7 @@ import java.util.ArrayList;
 public class ReminderReturn extends AppCompatActivity {
     public int day;
     public String[] checklist;
+    public String[] defaultList ={"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
